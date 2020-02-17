dc56dd624cb4bcecf237bd69c0f405d05f6b1beb
Dmytro Prylypa <59916712+Kungpao@users.noreply.github.com>
2020-02-17 15:19:58 +0300

    New_Delete_Main.cpp

---
+++ C++/Classes and Objects/Operator New Delete overload/New_Delete_Main.cpp
@@ -0,0 +1,14 @@
+// ConsoleApplication11.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include "A.h"
+
+int main()
+{
+
+	A *ptest = new A;
+	delete ptest;
+    return 0;
+}
+
