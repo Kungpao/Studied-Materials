fd2fc8509d19a8f44528df6d88827d210bf1aebf
Dmytro Prylypa <59916712+Kungpao@users.noreply.github.com>
2020-03-25 17:29:47 +0300

    Parrent_Class.cpp

---
+++ C++/Classes and Objects/Inheritance/Parrent_Class.cpp
@@ -0,0 +1,16 @@
+#include "stdafx.h"
+#include "DoubleGrafic.h"
+
+// body realization. setting default values to NULL
+Parrent::Parrent():a(0),b(0)
+{
+}
+//Setting params values
+Parrent::Parrent(const int &a, const int &b):a(a), b(b)
+{
+
+}
+//default destructor
+Parrent::~Parrent()
+{
+}
